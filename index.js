2eb7de02418d16ec5e2c9209477867bb3fd4da68
Carlos Diez <cdiezmoran@gmail.com>
2021-08-22 18:04:58 -0500

    fix script

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
-window.fbAsyncInit = function() {
+window.fbAsyncInit = function()
+{
     FB.init({
-        appId      : '1345101055833302',
-        status     : true,
-        xfbml      : true,
-        version    : 'v2.7' // or v2.6, v2.5, v2.4, v2.3
-    })
+        appId   : "221523695878917",
+        cookie  : true,  // enable cookies to allow the server to access
+                // the session
+        xfbml   : true,  // parse social plugins on this page
+        version : 'v11.0', // use version 2.1
+        status  : false
+    });
 
-    
+    FB.login(function(response) {
+      if (response.authResponse) {
+       console.log('Welcome!  Fetching your information.... ');
+       FB.api('/me', function(response) {
+         console.log('Good to see you, ' + response.name + '.');
+       });
+      } else {
+       console.log('User cancelled login or did not fully authorize.');
+      }
+    });
+
+    FB.getLoginStatus(function(response) {
+      if (response.status === 'connected') {
+        var accessToken = response.authResponse.accessToken;
+        console.log(accessToken)
+      } 
+    } )
+
+    // FB.login(function(response) { alert(response.authResponse.accessToken);
+    // }, {perms:'read_stream,publish_stream,offline_access'});
+
+    // FB.Event.subscribe('auth.statusChange', function(response) {
+    //     console.log( "FB.Event.subscribe auth.statusChange" );
+    //     console.log( response );
+    // });
+
+
+
+    // FB.getLoginStatus(function(response){
+    //     console.log( "FB.getLoginStatus" );
+    //     console.log( response );
+    // });
+
     FB.api(
-        '/me',
-        'GET',
-        {"fields":"id,name"},
-        function(response) {
-           console.log(response) // Insert your code here
-        }
-      );
-} 
+      '/23848541904820038/insights',
+      'GET',
+      {"fields":"impressions,clicks,ctr,spend,cpc","time_ranges":"[{since:'2021-07-21',until:'2021-08-01'}]"},
+      function(response) {
+          console.log(response)
+      }
+    );
+}
 
 console.log('hola')
