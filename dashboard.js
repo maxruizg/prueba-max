6c6d519a61b6fff9a2b89d1be6c90331cc11d7eb
maxruizg <maxruizg@gmail.com>
2021-08-23 16:12:25 -0500

    1

--- dashboard.js
+++ dashboard.js
@@ -1,40 +1,36 @@
-function Facebook() {
-    window.fbAsyncInit = function()
-    {
-        FB.init({
-            appId   : "1345101055833302",
-            cookie  : true,  // enable cookies to allow the server to access
-                    // the session
-            xfbml   : true,  // parse social plugins on this page
-            version : 'v11.0', // use version 2.1
-            status  : false
+window.fbAsyncInit = function()
+{
+    FB.init({
+        appId   : "1345101055833302",
+        cookie  : true,  // enable cookies to allow the server to access
+                // the session
+        xfbml   : true,  // parse social plugins on this page
+        version : 'v11.0', // use version 2.1
+        status  : false
+    });
+
+    FB.login(function(response) {
+        console.log('Running login');
+    if (response.authResponse) {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function(response) {
+        console.log('Good to see you, ' + response.name + '.');
         });
-
-        FB.login(function(response) {
-            console.log('Running login');
-        if (response.authResponse) {
-            console.log('Welcome!  Fetching your information.... ');
-            FB.api('/me', function(response) {
-            console.log('Good to see you, ' + response.name + '.');
-            });
-            
-            FB.api(
-            '/act_1468139590049416/campaigns',
-            'GET',
-            {"fields":"id"},
-            function(response) {
-                const idCampaigns = response
-            }
-            );
-
-        } else {
-        console.log('User cancelled login or did not fully authorize.');
+        
+        FB.api(
+        '/act_1468139590049416/campaigns',
+        'GET',
+        {"fields":"id"},
+        function(response) {
+            const idCampaigns = response
         }
-        });
-    }
-}
+        );
 
-Facebook()
+    } else {
+    console.log('User cancelled login or did not fully authorize.');
+    }
+    });
+}
 
 (() => {
     var ctx = document.getElementById('myChart1');
