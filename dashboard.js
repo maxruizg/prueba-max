beb408df435dcb3dd04167f6970c8846a561669d
maxruizg <maxruizg@gmail.com>
2021-08-23 14:32:06 -0500

    dashboard

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,39 @@
-(() => {
-    const p = new DataQueries([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
+window.fbAsyncInit = function()
+{
+    FB.init({
+        appId   : "1345101055833302",
+        cookie  : true,  // enable cookies to allow the server to access
+                // the session
+        xfbml   : true,  // parse social plugins on this page
+        version : 'v11.0', // use version 2.1
+        status  : false
+    });
 
+    FB.login(function(response) {
+        console.log('Running login');
+    if (response.authResponse) {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function(response) {
+        console.log('Good to see you, ' + response.name + '.');
+        });
+        
+        FB.api(
+        '/act_1468139590049416/campaigns',
+        'GET',
+        {"fields":"id"},
+        function(response) {
+            console.log(response)
+        }
+        );
+
+        
+    } else {
+    console.log('User cancelled login or did not fully authorize.');
+    }
+    });
+}
+
+(() => {
     var ctx = document.getElementById('myChart1');
     var myChart = new Chart(ctx, {
     type: 'line',
