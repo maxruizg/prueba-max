62a324b673cdb77da4f12760f9452c04ae7be7f3
maxruizg <maxruizg@gmail.com>
2021-08-22 22:00:17 -0500

    getLoginStatus

--- index.js
+++ index.js
@@ -17,39 +17,25 @@ window.fbAsyncInit = function()
          console.log('Good to see you, ' + response.name + '.');
        });
 
-       FB.api(
-        '/23848541904820038/insights',
-        'GET',
-        {"fields":"impressions,clicks,ctr,spend,cpc","time_ranges":"[{since:'2021-07-21',until:'2021-08-01'}]"},
-        function(response) {
-            console.log(response)
-        }
-      );
+        FB.api(
+          '/23848541904820038/insights',
+          'GET',
+          {"fields":"impressions,clicks,ctr,spend,cpc","time_ranges":"[{since:'2021-07-21',until:'2021-08-01'}]"},
+          function(response) {
+              console.log(response)
+          }
+        );
+        
       } else {
        console.log('User cancelled login or did not fully authorize.');
       }
     });
 
-    FB.getLoginStatus(function(response) {
-      if (response.status === 'connected') {
-        var accessToken = response.authResponse.accessToken;
-        console.log(accessToken)
-      } 
-    });
-
-    // FB.login(function(response) { alert(response.authResponse.accessToken);
-    // }, {perms:'read_stream,publish_stream,offline_access'});
-
-    // FB.Event.subscribe('auth.statusChange', function(response) {
-    //     console.log( "FB.Event.subscribe auth.statusChange" );
-    //     console.log( response );
-    // });
-
-
-
-    // FB.getLoginStatus(function(response){
-    //     console.log( "FB.getLoginStatus" );
-    //     console.log( response );
+    // FB.getLoginStatus(function(response) {
+    //   if (response.status === 'connected') {
+    //     var accessToken = response.authResponse.accessToken;
+    //     console.log(accessToken)
+    //   } 
     // });
 }
 
