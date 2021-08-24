624c92c80542855da070d4cf9b375177f19e6abe
maxruizg <maxruizg@gmail.com>
2021-08-24 09:41:47 -0500

    1

--- index.js
+++ index.js
@@ -76,8 +76,8 @@ window.fbAsyncInit = function()
                 'GET',
                 {"fields":"spend,clicks,impressions,ctr,cpc","time_ranges":"[{since:'2021-"+ mes +"-01',until:'2021-"+ mes + "-31'}]"},
                 function(response) {
-                  var obj = JSON.parse(response.data)
-                  console.log(obj.spend)
+                  var obj = JSON.parse(response)
+                  console.log(obj)
                 }
               );
 
