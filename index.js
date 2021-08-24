0e963805d856a17b9d3d404c63d6413107a3fc7a
maxruizg <maxruizg@gmail.com>
2021-08-24 13:58:55 -0500

    1

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 var totalSpend = 0
+// 23848541904780038
 
-var queries = (mes, element, startDay, endDay, numLocal, nombreLocal) => {
+// var queries = (mes, element, startDay, endDay, numLocal, nombreLocal) => {
+//   FB.api(
+//     '/' + element.id + '/insights',
+//     'GET',
+//     {"fields":"spend,clicks,impressions,ctr,cpc","time_ranges":"[{since:'2021-"+ mes +"-"+ startDay +"',until:'2021-"+ mes + "-"+ endDay +"'}]"},
+//     function(response) {
+//       console.log('querie')
+//       console.log(response)
+//       var string = JSON.stringify(response)
+//       var obj = JSON.parse(string)
+//       var data = obj.data
+      
+//       if(data[0] != undefined){
+//         var spend = data[0].spend
+//         var spend1 = parseFloat(spend)
+//         totalSpend += spend1
+//         localStorage.setItem(`${nombreLocal}_${numLocal}`, JSON.stringify(totalSpend));
+//       }
+//     }
+//   );
+// }
+
+var queries = (mes, startDay, endDay, numLocal, nombreLocal) => {
   FB.api(
-    '/' + element.id + '/insights',
+    '/23848541904780038/insights',
     'GET',
     {"fields":"spend,clicks,impressions,ctr,cpc","time_ranges":"[{since:'2021-"+ mes +"-"+ startDay +"',until:'2021-"+ mes + "-"+ endDay +"'}]"},
     function(response) {
@@ -53,32 +76,34 @@ window.fbAsyncInit = function()
             var mes = parseInt(months[0]) + 1
             var mesAnterior = parseInt(months[0])
 
-            idCampaigns.forEach(element => {
+            queries(mes, '1', '8', 'Spend')
 
-              queries(mes, element, '1', '8', 1, 'Spend')
+            // idCampaigns.forEach(element => {
 
-            });
+            //   queries(mes, element, '1', '8', 1, 'Spend')
 
-            totalSpend = 0
-            idCampaigns.forEach(element => {
+            // });
 
-              queries(mes, element, '9', '15', 2, 'Spend')
+            // totalSpend = 0
+            // idCampaigns.forEach(element => {
 
-            });
+            //   queries(mes, element, '9', '15', 2, 'Spend')
 
-            totalSpend = 0
-            idCampaigns.forEach(element => {
+            // });
 
-              queries(mes, element, '16', '24', 3, 'Spend')
+            // totalSpend = 0
+            // idCampaigns.forEach(element => {
 
-            });
+            //   queries(mes, element, '16', '24', 3, 'Spend')
 
-            totalSpend = 0
-            idCampaigns.forEach(element => {
+            // });
 
-              queries(mes, element, '25', '31', 4, 'Spend')
+            // totalSpend = 0
+            // idCampaigns.forEach(element => {
 
-            });
+            //   queries(mes, element, '25', '31', 4, 'Spend')
+
+            // });
           }
         );
 
