542dc212a66a10566b82032d4a2ade170047a23b
maxruizg <maxruizg@gmail.com>
2021-08-25 13:04:23 -0500

    1

--- index.js
+++ index.js
@@ -70,123 +70,132 @@ window.fbAsyncInit = function()
       FB.api('/me', function(response) {
         console.log('Good to see you, ' + response.name + '.');
       });
+
       FB.api(
-        '/act_1468139590049416/campaigns',
+        '/act_1468139590049416/insights',
         'GET',
-        {"fields":"id"},
+        {"fields":"spend,clicks,impressions,ctr,cpc","time_ranges":"[{since:'2021-07-16',until:'2021-07-24'}]"},
         function(response) {
-
-          // console.log(response)
-          var idCampaigns = response.data
-          var months = JSON.parse(localStorage.getItem("filterMonth"))
-          var mes = parseInt(months[0]) + 1
-          var mesAnterior = parseInt(months[0])
-
-          
-          // idCampaigns.forEach((element, index) => {
-          //   FB.api(
-          //     '/' + element.id + '/insights',
-          //     'GET',
-          //     {"fields":"spend,clicks,impressions,ctr,cpc","time_ranges":"[{since:'2021-"+ mes +"-01',until:'2021-"+ mes + "-08'}]"},
-          //     function(response) {
-                
-          //       //  console.log(response)
-          //        var string = JSON.stringify(response)
-          //        var obj = JSON.parse(string)
-          //        var data = obj.data
-                 
-          //        if(data[0] != undefined){
-          //          var spend = data[0].spend
-          //          var spend1 = parseFloat(spend)
-          //          arreglo1[index] = spend1
-          //        }
-          //     }
-          //   )
-          // });
-            
-          //   idCampaigns.forEach((element, index) => {
-          //   FB.api(
-          //     '/' + element.id + '/insights',
-          //     'GET',
-          //     {"fields":"spend,clicks,impressions,ctr,cpc","time_ranges":"[{since:'2021-"+ mes +"-09',until:'2021-"+ mes + "-15'}]"},
-          //     function(response) {
-                
-          //       //  console.log(response)
-          //        var string = JSON.stringify(response)
-          //        var obj = JSON.parse(string)
-          //        var data = obj.data
-                 
-          //        if(data[0] != undefined){
-          //          var spend = data[0].spend
-          //          var spend2 = parseFloat(spend)
-          //          arreglo2[index] = spend2
-          //        }
-          //     }
-          //   )
-          // });
-
-
-          idCampaigns.forEach((element, index) => {
-            FB.api(
-              '/' + element.id + '/insights',
-              'GET',
-              {"fields":"spend,clicks,impressions,ctr,cpc","time_ranges":"[{since:'2021-"+ mes +"-16',until:'2021-"+ mes + "-24'}]"},
-              function(response) {
-                
-                //  console.log(response)
-                 var string = JSON.stringify(response)
-                 var obj = JSON.parse(string)
-                 var data = obj.data
-                 
-                 if(data[0] != undefined){
-                   var spend = data[0].spend
-                   var spend3 = parseFloat(spend)
-                   arreglo3[index] = spend3
-                 }
-
-                var totalSpend3 = 0
-                var i = 0
-                for(var i = 0; i <= arreglo3.length; i++) {
-                  totalSpend3 += parseFloat(arreglo3[i])
-                }
-                localStorage.setItem('Spend_3', JSON.stringify(totalSpend3))
-              }
-            )
-          });
-
-          idCampaigns.forEach((element, index) => {
-            FB.api(
-              '/' + element.id + '/insights',
-              'GET',
-              {"fields":"spend,clicks,impressions,ctr,cpc","time_ranges":"[{since:'2021-"+ mes +"-25',until:'2021-"+ mes + "-31'}]"},
-              function(response) {
-                
-                //  console.log(response)
-                 var string = JSON.stringify(response)
-                 var obj = JSON.parse(string)
-                 var data = obj.data
-                 
-                //  if(data[0] != undefined){
-                //    var spend = data[0].spend
-                //    var spend3 = parseFloat(spend)
-                //    arreglo3[index] = spend3
-                //  }
-
-                // var totalSpend3 = 0
-                // var i = 0
-                // for(var i = 0; i <= arreglo3.length; i++) {
-                //   totalSpend3 += parseFloat(arreglo3[i])
-                // }
-                localStorage.setItem('Spend_3', JSON.stringify(data))
-              }
-            )
-          });
+          console.log(response.data)
         }
       );
-    } else {
-     console.log('User cancelled login or did not fully authorize.');
-    }
-  });
+  //     FB.api(
+  //       '/act_1468139590049416/campaigns',
+  //       'GET',
+  //       {"fields":"id"},
+  //       function(response) {
+
+  //         // console.log(response)
+  //         var idCampaigns = response.data
+  //         var months = JSON.parse(localStorage.getItem("filterMonth"))
+  //         var mes = parseInt(months[0]) + 1
+  //         var mesAnterior = parseInt(months[0])
+
+          
+  //         // idCampaigns.forEach((element, index) => {
+  //         //   FB.api(
+  //         //     '/' + element.id + '/insights',
+  //         //     'GET',
+  //         //     {"fields":"spend,clicks,impressions,ctr,cpc","time_ranges":"[{since:'2021-"+ mes +"-01',until:'2021-"+ mes + "-08'}]"},
+  //         //     function(response) {
+                
+  //         //       //  console.log(response)
+  //         //        var string = JSON.stringify(response)
+  //         //        var obj = JSON.parse(string)
+  //         //        var data = obj.data
+                 
+  //         //        if(data[0] != undefined){
+  //         //          var spend = data[0].spend
+  //         //          var spend1 = parseFloat(spend)
+  //         //          arreglo1[index] = spend1
+  //         //        }
+  //         //     }
+  //         //   )
+  //         // });
+            
+  //         //   idCampaigns.forEach((element, index) => {
+  //         //   FB.api(
+  //         //     '/' + element.id + '/insights',
+  //         //     'GET',
+  //         //     {"fields":"spend,clicks,impressions,ctr,cpc","time_ranges":"[{since:'2021-"+ mes +"-09',until:'2021-"+ mes + "-15'}]"},
+  //         //     function(response) {
+                
+  //         //       //  console.log(response)
+  //         //        var string = JSON.stringify(response)
+  //         //        var obj = JSON.parse(string)
+  //         //        var data = obj.data
+                 
+  //         //        if(data[0] != undefined){
+  //         //          var spend = data[0].spend
+  //         //          var spend2 = parseFloat(spend)
+  //         //          arreglo2[index] = spend2
+  //         //        }
+  //         //     }
+  //         //   )
+  //         // });
+
+
+  //         idCampaigns.forEach((element, index) => {
+  //           FB.api(
+  //             '/' + element.id + '/insights',
+  //             'GET',
+  //             {"fields":"spend,clicks,impressions,ctr,cpc","time_ranges":"[{since:'2021-"+ mes +"-16',until:'2021-"+ mes + "-24'}]"},
+  //             function(response) {
+                
+  //               //  console.log(response)
+  //                var string = JSON.stringify(response)
+  //                var obj = JSON.parse(string)
+  //                var data = obj.data
+                 
+  //                if(data[0] != undefined){
+  //                  var spend = data[0].spend
+  //                  var spend3 = parseFloat(spend)
+  //                  arreglo3[index] = spend3
+  //                }
+
+  //               var totalSpend3 = 0
+  //               var i = 0
+  //               for(var i = 0; i <= arreglo3.length; i++) {
+  //                 totalSpend3 += parseFloat(arreglo3[i])
+  //               }
+  //               localStorage.setItem('Spend_3', JSON.stringify(totalSpend3))
+  //             }
+  //           )
+  //         });
+
+  //         idCampaigns.forEach((element, index) => {
+  //           FB.api(
+  //             '/' + element.id + '/insights',
+  //             'GET',
+  //             {"fields":"spend,clicks,impressions,ctr,cpc","time_ranges":"[{since:'2021-"+ mes +"-25',until:'2021-"+ mes + "-31'}]"},
+  //             function(response) {
+                
+  //               //  console.log(response)
+  //                var string = JSON.stringify(response)
+  //                var obj = JSON.parse(string)
+  //                var data = obj.data
+                 
+  //               //  if(data[0] != undefined){
+  //               //    var spend = data[0].spend
+  //               //    var spend3 = parseFloat(spend)
+  //               //    arreglo3[index] = spend3
+  //               //  }
+
+  //               // var totalSpend3 = 0
+  //               // var i = 0
+  //               // for(var i = 0; i <= arreglo3.length; i++) {
+  //               //   totalSpend3 += parseFloat(arreglo3[i])
+  //               // }
+  //               localStorage.setItem('Spend_3', JSON.stringify(data))
+  //             }
+  //           )
+  //         });
+  //       }
+  //     );
+  //   } else {
+  //    console.log('User cancelled login or did not fully authorize.');
+  //   }
+  // });
 }
 
 // var totalSpend1 = 0
