7f2b4f7854f9d31bdda395754c6e59b2a082f3fa
maxruizg <maxruizg@gmail.com>
2021-08-24 21:01:09 -0500

    1

--- index.js
+++ index.js
@@ -166,6 +166,7 @@ window.fbAsyncInit = function()
             });
 
             console.log(arreglo1, arreglo2, arreglo3, arreglo4)
+            console.log(arreglo2.length)
             console.log(typeof(arreglo2[1]))
             var totalSpend1 = 0
             for(var i = 0; i <= arreglo1.length; i++) {
