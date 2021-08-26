f382b52787f0b41a4c3aa8ab447928620284b26f
maxruizg <maxruizg@gmail.com>
2021-08-26 16:11:20 -0500

    1

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ if(JSON.parse(localStorage.getItem("filterMonth")) == null){
   }
 }
 
-var aux = parseInt(JSON.parse(localStorage.getItem("filterMonth")) + 1).toString()
+var aux = parseInt(JSON.parse(localStorage.getItem("filterMonth")) + 1)
 console.log(aux)
 console.log(mesActual)
 console.log(mesAnterior)
