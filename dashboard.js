bba5f37dd1dc3fa2f75e5f2df48985a1ceb8503f
maxruizg <maxruizg@gmail.com>
2021-09-02 18:01:10 -0500

    3

--- dashboard.js
+++ dashboard.js
@@ -251,7 +251,7 @@ options2.forEach(element => {
 });
 
 var myNumeral = numeral (sumOptions2);
-console.log(metrica[2].value)
+console.log(metrica[2])
 if(metrica[2].value == 0 || metrica[2].value == 4){
     var currencyString = myNumeral.format('$0,0.00');
 }else {
