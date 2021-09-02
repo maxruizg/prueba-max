a75dcc963ea54f5f423255f5e91b6f82bc3e0958
maxruizg <maxruizg@gmail.com>
2021-09-02 10:15:26 -0500

    3

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ window.fbAsyncInit = function()
 
        //Datos mes actual
         FB.api(
-          '/act_1468139590049416/insights',
+          '/act_704269000261751/insights',
           'GET',
           {"fields":"spend,clicks,impressions,ctr,cpc","time_ranges":"[{since:'2021-"+ mesActual + "-01',until:'2021-"+ mesActual +"-08'}]"},
           function(response) {
@@ -55,7 +55,7 @@ window.fbAsyncInit = function()
         );
 
         FB.api(
-          '/act_1468139590049416/insights',
+          '/act_704269000261751/insights',
           'GET',
           {"fields":"spend,clicks,impressions,ctr,cpc","time_ranges":"[{since:'2021-"+ mesActual +"-09',until:'2021-"+ mesActual +"-15'}]"},
           function(response) {
@@ -68,7 +68,7 @@ window.fbAsyncInit = function()
         );
 
         FB.api(
-          '/act_1468139590049416/insights',
+          '/act_704269000261751/insights',
           'GET',
           {"fields":"spend,clicks,impressions,ctr,cpc","time_ranges":"[{since:'2021-"+ mesActual +"-16',until:'2021-0"+ mesActual +"-24'}]"},
           function(response) {
@@ -81,7 +81,7 @@ window.fbAsyncInit = function()
         );
 
         FB.api(
-          '/act_1468139590049416/insights',
+          '/act_704269000261751/insights',
           'GET',
           {"fields":"spend,clicks,impressions,ctr,cpc","time_ranges":"[{since:'2021-"+ mesActual +"-25',until:'2021-"+ mesActual +"-31'}]"},
           function(response) {
@@ -96,7 +96,7 @@ window.fbAsyncInit = function()
 
         // Datos mes anterior
         FB.api(
-          '/act_1468139590049416/insights',
+          '/act_704269000261751/insights',
           'GET',
           {"fields":"spend,clicks,impressions,ctr,cpc","time_ranges":"[{since:'2021-"+ mesAnterior + "-01',until:'2021-"+ mesAnterior +"-08'}]"},
           function(response) {
@@ -109,7 +109,7 @@ window.fbAsyncInit = function()
         );
 
         FB.api(
-          '/act_1468139590049416/insights',
+          '/act_704269000261751/insights',
           'GET',
           {"fields":"spend,clicks,impressions,ctr,cpc","time_ranges":"[{since:'2021-"+ mesAnterior +"-09',until:'2021-"+ mesAnterior +"-15'}]"},
           function(response) {
@@ -124,7 +124,7 @@ window.fbAsyncInit = function()
 
         
         FB.api(
-          '/act_1468139590049416/insights',
+          '/act_704269000261751/insights',
           'GET',
           {"fields":"spend,clicks,impressions,ctr,cpc","time_ranges":"[{since:'2021-"+ mesAnterior +"-16',until:'2021-0"+ mesAnterior +"-24'}]"},
           function(response) {
@@ -137,7 +137,7 @@ window.fbAsyncInit = function()
         );
 
         FB.api(
-          '/act_1468139590049416/insights',
+          '/act_704269000261751/insights',
           'GET',
           {"fields":"spend,clicks,impressions,ctr,cpc","time_ranges":"[{since:'2021-"+ mesAnterior +"-25',until:'2021-"+ mesAnterior +"-31'}]"},
           function(response) {
