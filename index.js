d237c7b9d77a6d0c1598d3da53e58096f3147463
maxruizg <maxruizg@gmail.com>
2021-09-03 12:14:58 -0500

    3

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ window.fbAsyncInit = function()
            'GET',
            {"fields":"spend,clicks,impressions,ctr,cpc,frequency","time_ranges":"[{since:'"+ year +"-"+ mesActual + "-01',until:'"+ year +"-"+ mesActual +"-08'}]"},
            function(response) {
+             console.log(response)
              if(response.data[0] != undefined || parseInt(mesActual) > d.getMonth){
                localStorage.setItem('Semana-1', JSON.stringify(response.data[0]))
              }else {
