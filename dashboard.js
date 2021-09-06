1239bcfe0c32adb25fd86eb4e848c71a7db28597
maxruizg <maxruizg@gmail.com>
2021-09-06 17:22:13 -0500

    5

--- dashboard.js
+++ dashboard.js
@@ -200,33 +200,6 @@ function ArregloDatos(metrica, semanas){
         chart.update()
     }
 
-    const selectMetrica = document.getElementById('selectMetrica1')
-    selectMetrica.addEventListener('change', (event, myChart) => {
-        const valorMetrica = event.value
-        let metrica = []
-        if(JSON.parse(localStorage.getItem("filterData")) == null){
-            metrica[0] = 0
-            metrica[1] = 0
-            metrica[2] = 0
-            metrica[3] = 0
-            metrica[4] = 0
-            metrica[5] = 0
-            metrica[6] = 0
-            metrica[7] = 0
-            metrica[8] = 0
-            metrica[9] = 0
-        }else {
-            metrica = JSON.parse(localStorage.getItem("filterData"))
-        }
-
-        metrica[0] = valorMetrica
-        localStorage.setItem('selectMetrica1', JSON.stringify(metrica))
-
-        let datosGrafica1Actual = ArregloDatos(metrica[0], semanas)
-        let datosGrafica1Anterior = ArregloDatos(metrica[0], semanasA) 
-
-        addData(myChart, datosGrafica1Actual, datosGrafica1Anterior)
-    })
 })();
 
 
@@ -643,3 +616,28 @@ function ActualizacionAds() {
     DatosTop(metrica, 9, adsData, 'nombreAd', 'valorAd')
     localStorage.setItem('filterData', JSON.stringify(metrica))
 }
+
+const selectMetrica = document.getElementById('selectMetrica1')
+selectMetrica.addEventListener('change', (event) => {
+    const valorMetrica = event.value
+    let metrica = []
+    if(JSON.parse(localStorage.getItem("filterData")) == null){
+        metrica[0] = 0
+        metrica[1] = 0
+        metrica[2] = 0
+        metrica[3] = 0
+        metrica[4] = 0
+        metrica[5] = 0
+        metrica[6] = 0
+        metrica[7] = 0
+        metrica[8] = 0
+        metrica[9] = 0
+    }else {
+        metrica = JSON.parse(localStorage.getItem("filterData"))
+    }
+
+    metrica[0] = valorMetrica
+    localStorage.setItem('selectMetrica1', JSON.stringify(metrica))
+
+    location.reload()
+})
