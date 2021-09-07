0805686b9f005432b663d7bc349fadaf36d5e792
maxruizg <maxruizg@gmail.com>
2021-09-06 19:54:42 -0500

    5

--- dashboard.js
+++ dashboard.js
@@ -631,27 +631,10 @@ function ActualizacionAds() {
     localStorage.setItem('filterData', JSON.stringify(metrica))
 }
 
-const elementoMetrica = document.getElementById('selectMetrica1')
+const elementoMetrica = document.getElementById('selectMonth')
 elementoMetrica.addEventListener('change', (event) => {
     console.log('entro')
     const valorMetrica = event.target.value
-    let metrica = []
-    if(JSON.parse(localStorage.getItem("filterData")) == null){
-        metrica[0] = 0
-        metrica[1] = 0
-        metrica[2] = 0
-        metrica[3] = 0
-        metrica[4] = 0
-        metrica[5] = 0
-        metrica[6] = 0
-        metrica[7] = 0
-        metrica[8] = 0
-        metrica[9] = 0
-    }else {
-        metrica = JSON.parse(localStorage.getItem("filterData"))
-    }
-
-    metrica[0] = valorMetrica
-    localStorage.setItem('filterData', JSON.stringify(metrica))
+    localStorage.setItem('filterMonth', JSON.stringify(valorMetrica))
     location.reload()
 })
