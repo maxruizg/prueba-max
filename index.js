33e135e1d8541a7dcf98129c13741ddf0806d29d
maxruizg <maxruizg@gmail.com>
2021-09-13 12:39:30 -0500

    saveMetricas

--- index.js
+++ index.js
@@ -239,8 +239,8 @@ window.fbAsyncInit = function()
       } else {
        console.log('User cancelled login or did not fully authorize.');
       }
-      saveMetrics()
     });
+    saveMetrics()
 }
 
 async function Cargar() {
